46b0523477e6100f6399e636ebb2094c51dcdb26
Jared Baur <jaredbaur@fastmail.com>
2022-09-30 10:54:02 -0700

    Add gitignores

--- golang/flake.nix
+++ golang/flake.nix
@@ -3,6 +3,8 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
   outputs = inputs: with inputs; {
     overlays.default = _: prev: {
@@ -15,10 +17,22 @@
         inherit system;
         overlays = [ self.overlays.default ];
       };
+      preCommitHooks = pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          nixpkgs-fmt.enable = true;
+          gofmt = {
+            enable = true;
+            entry = "${pkgs.gobar.go}/bin/gofmt -w";
+            types = [ "go" ];
+          };
+        };
+      };
     in
     {
       devShells.default = pkgs.mkShell {
         inherit (pkgs.TODO) CGO_ENABLED nativeBuildInputs;
+        inherit (preCommitHooks) shellHook;
       };
       packages.default = pkgs.TODO;
     });
--- rust/flake.nix
+++ rust/flake.nix
@@ -3,6 +3,8 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
   outputs = inputs: with inputs; {
     overlays.default = _: prev: {
@@ -15,10 +17,20 @@
         inherit system;
         overlays = [ self.overlays.default ];
       };
+      preCommitHooks = pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          cargo-check.enable = true;
+          clippy.enable = true;
+          nixpkgs-fmt.enable = true;
+          rustfmt.enable = true;
+        };
+      };
     in
     {
       devShells.default = pkgs.mkShell {
         inherit (pkgs.TODO) RUSTFLAGS nativeBuildInputs;
+        inherit (preCommitHooks) shellHook;
       };
       packages.default = pkgs.TODO;
     });
