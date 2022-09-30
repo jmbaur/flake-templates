b3499503082b91714c71c0d151320b1055af0c61
Jared Baur <jaredbaur@fastmail.com>
2022-09-30 10:48:24 -0700

    Add nixpkgs-fmt

--- flake.nix
+++ flake.nix
@@ -1,12 +1,28 @@
 {
   description = "Flake Templates";
 
-  outputs = _: {
-    templates = {
-      golang = {
-        description = "golang";
-        path = ./golang;
-      };
-    };
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
+
+  outputs = inputs: with inputs; {
+    templates = {
+      golang = { description = "golang"; path = ./golang; };
+      rust = { description = "rust"; path = ./rust; };
+    };
+  }
+  // flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      preCommitHooks = pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks.nixpkgs-fmt.enable = true;
+      };
+    in
+    {
+      devShells.default = pkgs.mkShell { inherit (preCommitHooks) shellHook; };
+    });
 }
